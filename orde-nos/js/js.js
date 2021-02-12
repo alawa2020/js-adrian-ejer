2e4005f7b0dd91eb3b4b44d4231aa1960d8446fb
stargazing20 <huder13168@gmail.com>
2021-02-12 18:50:25 -0500

    Agregando la logica

--- orde-nos/js/js.js
+++ orde-nos/js/js.js
@@ -1,5 +1,4 @@
 const numbers = document.getElementById("numbers");
-const suma= document.getElementById("suma");
 const result = document.getElementById("result");
 
 //PIDIENDO VALORES EN EL HTML
@@ -10,7 +9,37 @@ let c = prompt("Introduzca el número 3");
 //SUMANDO
 let su = parseInt(a)+parseInt(b)+parseInt(c);
 
+//LOGICA PARA SUMAR
+let n1,n2,n3;
+
+if (a<b){
+    if(b<c){
+        n1=a;n2=b;n3=c;
+    }
+    else{
+        if(a<c){
+            n1=a;n2=c;n3=b;
+        }
+        else{
+            n1=c;n2=a;n3=b;
+        }
+    }
+}
+else{
+    if(c<b){
+        n1=c;n2=b;n3=a;
+    }
+    else{
+        if(a<c){
+            n1=b;n2=a;n3=c;
+        }
+        else{
+            n1=b;n2=c;n3=a;
+        }
+    }
+}
+
 
 //MOSTRANDO LOS RESULTADOS EN EL HTML
-numbers.textContent = `Los números son: ${a}, ${b} y ${c}.`
-suma.textContent = `La suma de estos números es: ${su}`
+numbers.textContent = `Los números son: ${a}, ${b} y ${c}.`;
+result.textContent=`Los números ordenados son : ${n1}, ${n2},${n3}`;
